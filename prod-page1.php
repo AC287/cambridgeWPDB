536c118c7c2480b467d9eb74abecd33cd72f1225
AC287 <arthurchen287@gmail.com>
2017-08-08 16:32:16 -0400

    updated accordion bar to p2 p3 and pitem.

--- prod-page1.php
+++ prod-page1.php
@@ -40,28 +40,29 @@ get_header(); ?>
 				// print_r(sizeof($s1_category));
 				// print_r($s1_category[0]->s1);
 				if(!empty($s1_category[0]->s1)){
-					echo "<div>";
-					echo "<button class='accordion'>".$main_category->m0."</button>";
+					// echo "<div>";
+					echo "<div class='accordion'><img class='chev' src='http://files.coda.com.s3.amazonaws.com/imgv2/icons/chev-right.png'>&nbsp".$main_category->m0."</div>";
 					echo "<div class='panel'>";
 					foreach($s1_category as $s1_category) {
-						echo "<button class='accordion'>".$s1_category->s1."</button>";
 						$s2_category = $wpdb->get_results("SELECT DISTINCT s2 FROM wp_prod0 WHERE s1 = '$s1_category->s1';");
 						if(!empty($s2_category[0]->s2)){
+							echo "<div class='accordion'><img class='chev' src='http://files.coda.com.s3.amazonaws.com/imgv2/icons/chev-right.png'>&nbsp".$s1_category->s1."</div>";
 							echo "<div class='panel'>";
 							foreach($s2_category as $s2_category) {
-								echo "<button class='accordion'>".$s2_category->s2."</button>";
+								echo "<div class='accordion no-sub'><a class='no-sub' href='products/ps2/?m0=".$main_category->m0."&s1=".$s1_category->s1."&s2=".$s2_category->s2."'>".$s2_category->s2."</a></div>";
 							}
-							echo "</div>";
+							echo "</div>";  // end panel
+						} else {
+							echo "<div class='accordion'><a class='no-sub' href='products/ps2/?m0=".$main_category->m0."&s1=".$s1_category->s1."&s2=".$s2_category->s2."'>".$s1_category->s1."</a></div>";
 						}
 					}
-					echo "</div>";
+					echo "</div>";  // end panel.
 				}
 				else {
-					echo "<div>";
-					echo "<button class='accordion'>".$main_category->m0."</button>";
+					echo "<div class='accordion'>".$main_category->m0."</div>";
 				}
 				// echo "<hr/>";
-				echo "</div>";
+				// echo "</div>";
 			}
 			echo "</td>";
 			// END Main Category accordion panel.
